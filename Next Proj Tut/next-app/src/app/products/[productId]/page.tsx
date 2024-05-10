2e2358b304efb387e1e0f2250d85710fe5746a5a
NizarZouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-05-10 03:18:43 +0100

    Layouts, SEO & Metadata

--- Next Proj Tut/next-app/src/app/products/[productId]/page.tsx
+++ Next Proj Tut/next-app/src/app/products/[productId]/page.tsx
@@ -1,3 +1,17 @@
+import { Metadata } from "next"
+
+export const generateMetadata = ({params}: Props): Metadata => { // can also be defined as async function
+  return {
+    title: `Product ${params.productId}`
+  }
+}
+
+type Props = {
+  params: {
+    productId: String
+  }
+}
+
 export default function ProductDetails({params}: {params:{productId:string}}) {
   return (
     <div>
